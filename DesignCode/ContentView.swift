c32155698c5f5dade4110d2e1591fa4eb7164103
Patrick <patrick.ortell@arus.io>
2021-07-09 07:47:02 -0600

    Gestures and Events

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 struct ContentView: View {
     @State var show = false
+    @State var viewState = CGSize.zero
 
     
     var body: some View {
@@ -21,6 +22,8 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
+
                 .scaleEffect(0.9)
                 .rotationEffect(Angle.degrees(show ? 0 : 10))
                 .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
@@ -32,16 +35,31 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
+
                 .scaleEffect(0.95)
                 .rotationEffect(Angle.degrees(show ? 0 : 5))
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3))
+            
             CardView()
+                .offset(x: viewState.width, y: viewState.height)
                 .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
                     self.show.toggle()
                 }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                        self.show = true
+                    }
+                    .onEnded { value in
+                        self.viewState = .zero
+                        self.show = false
+                    }
+                )
             
             BottomCardView()
                 .blur(radius: show ? 20 : 0)
@@ -135,6 +153,6 @@ struct BottomCardView: View {
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: 500)
+        .offset(x: 0, y: 560)
     }
 }
