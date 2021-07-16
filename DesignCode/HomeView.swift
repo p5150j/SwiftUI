b34081e164cc0697bd1782e67a3840e0d7263519
Patrick <patrick.ortell@arus.io>
2021-07-16 10:44:17 -0600

    ScrollView and Repeat

---
+++ DesignCode/HomeView.swift
@@ -0,0 +1,73 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by Patrick Ortell on 7/16/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                Spacer()
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.leading, 14)
+            .padding(.top, 30)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                        SectionView()
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype designs in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image("Logo1")
+            }
+            Text("18 sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
+    }
+}
