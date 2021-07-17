fda1c7c65f82e7823a8f66aa900870aa657146b6
Patrick <patrick.ortell@arus.io>
2021-07-16 22:49:03 -0600

    Ring Progress Circle
    Apple Watch-like using trim, stroke and rotation

---
+++ DesignCode/TabBar.swift
@@ -0,0 +1,30 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by Patrick Ortell on 7/16/21.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Cerifiascats")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
