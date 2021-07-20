d955f4d09b64b868f52beb90dca628102718820d
Patrick <patrick.ortell@arus.io>
2021-07-20 15:33:51 -0600

    Contentful and Combine

--- DesignCode/CourseStore.swift
+++ DesignCode/CourseStore.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 import Contentful
+import Combine
 
 let clinet = Client(spaceId: "5wndy7owmgmk", accessToken: "d-XLNaaU_LS3obTsyXSXvWNgj6YE_Dbc9_t4u6Pxieo")
 
 
-func getArray() {
-    let query = Query.where(contentTypeId: "course")
+func getArray(id: String, completion: @escaping([Entry]) -> ()) {
+    let query = Query.where(contentTypeId: id)
     
     clinet.fetchArray(of: Entry.self, matching: query) { result in
-        print(result)
-        print("wtf")
+//        print(result)
+        switch result {
+        case .success(let array):
+            DispatchQueue.main.async {
+                completion(array.items)
+            }
+            
+        case .failure(let failure):
+            print(failure)
+        }
+    }
+}
+
+class CourseStore: ObservableObject {
+    @Published var courses: [Course] = courseData
+    
+    init() {
+        getArray(id: "course") { items in
+            items.forEach { item in
+                self.courses.append(Course(
+                                        title: item.fields["title"] as! String,
+                                        subtitle: item.fields["subtitle"] as! String,
+                                        image: #imageLiteral(resourceName: "Avatar"),
+                                        logo: #imageLiteral(resourceName: "Logo1"),
+                                        color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1),
+                                        show: false
+                ))
+            }
+        }
     }
 }
